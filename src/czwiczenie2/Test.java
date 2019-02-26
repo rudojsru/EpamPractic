d3d810ba07338d1b90376346a5ded9635600fcff
rudojsru <rudojsru@gmail.com>
2019-02-26 17:03:07 +0100

    Corrected the end

--- src/czwiczenie2/Test.java
+++ src/czwiczenie2/Test.java
@@ -1,7 +1,5 @@
 package czwiczenie2;
 
-import java.util.Arrays;
-
 public class Test {
 
     public static void main(String[] args) {
--- src/czwiczenie2/TextReader.java
+++ src/czwiczenie2/TextReader.java
@@ -1,5 +1,6 @@
 package czwiczenie2;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -14,12 +15,16 @@ public class TextReader {
     //a method for reading the full fail
    public void linesReader() {
         try {
-
-             lines = Files.readAllLines(Paths.get("Readmy.txt"), StandardCharsets.UTF_8);
+            final String TEXT = trackToFiles()+"Riddle";    //
+             lines = Files.readAllLines(Paths.get("Riddle"), StandardCharsets.UTF_8);
            // System.out.println(lines);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("error");
+            System.out.println("File not fount");
+        }
+        if (lines==null){
+            System.out.println("File is empty ");
+            return;
         }
     }
 
@@ -66,5 +71,24 @@ public class TextReader {
         System.out.println(list.get(numberFromRandom));
 
 
+    }
+// to run the file from anywhere
+    public static String trackToFiles() {
+        String track = new File(".").getAbsolutePath();
+        // System.out.println(track);
+
+        String[] splitTrack = track.split("");
+        //   System.out.println(splitTrack.length);
+        track = "";
+
+
+        for (int i = 0; i < splitTrack.length - 1; i++) {
+            track += splitTrack[i];
+
+        }
+
+
+        System.out.println(track);
+        return track;
     }
 }
